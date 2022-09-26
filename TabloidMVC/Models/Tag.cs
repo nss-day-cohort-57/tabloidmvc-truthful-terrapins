231652a8f510c0275a8b7b6a3bbb9b37e0fbee75
Robert Parker <rober.m.parker.j@gmail.com>
2022-09-26 13:49:36 -0500

    View all Tags

---
+++ TabloidMVC/Models/Tag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TabloidMVC.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
